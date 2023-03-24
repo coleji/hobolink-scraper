fd4e44eb709ed650f5804d2719c6073ecca5a013
Jonathan Cole <jon@jcole.io>
2023-03-23 20:43:38 -0400

    error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-// const fs = require('fs')
+const fs = require('fs')
 const parse = require("./parse")
 const httpCall = require("./http-call")
 
-httpCall().then(data => {
-	const readings = parse(data)
-	readings.forEach(({ label, reading, units }) => console.log(`"${label}": "${reading}" "${units}"`))
-})
+const FREQ = 1000 * 60 * 5
+
+function main() {
+	httpCall().then(data => {
+		return parse(data)
+	}).then(readings => {
+		const out = readings.map(({ label, reading, units }) => `"${label}","${reading}","${units}"`).join("\n")
+		fs.writeFileSync("./out/readings.csv", out)
+		console.log(new Date())
+		console.log(out)
+	}).catch(err => {
+		console.error("Error: ", err)
+		try {fs.unlinkSync("./out/readings.csv")} catch (e) {}
+	})
+
+	setTimeout(main, FREQ)
+}
+
+main()
--- src/parse.js
+++ src/parse.js
@@ -1,17 +1,23 @@
 const parse = require('node-html-parser').parse;
 
 module.exports = function(input) {
-	const root = parse(input)
-	const tbody = root.querySelector("#hobolink-latest-conditions-form\\:conditions-tree_data")
-	return tbody.childNodes.filter((e, i) => i > 0 && i < 11).map(tr => {
-		const label = tr.querySelector(".latest-conditions-info-label").childNodes[0]['_rawText'].split(":")[0]
-		const readingNode = tr.querySelector(".latest-conditions-info-reading").childNodes[0]
-		const reading = (
-			readingNode.childNodes.length == 0
-			? readingNode['_rawText']
-			: readingNode.childNodes[0]['_rawText']
-		)
-		const units = tr.querySelector(".latest-conditions-info-units").childNodes[0]['_rawText']
-		return { label, reading, units }
+	return new Promise((resolve, reject) => {
+		try {
+			const root = parse(input)
+			const tbody = root.querySelector("#hobolink-latest-conditions-form\\:conditions-tree_data")
+			resolve(tbody.childNodes.filter((e, i) => i > 0 && i < 11).map(tr => {
+				const label = tr.querySelector(".latest-conditions-info-label").childNodes[0]['_rawText'].split(":")[0]
+				const readingNode = tr.querySelector(".latest-conditions-info-reading").childNodes[0]
+				const reading = (
+					readingNode.childNodes.length == 0
+					? readingNode['_rawText']
+					: readingNode.childNodes[0]['_rawText']
+				)
+				const units = tr.querySelector(".latest-conditions-info-units").childNodes[0]['_rawText']
+				return { label, reading, units }
+			}));
+		} catch (e) {
+			reject(e)
+		}
 	})
 }
