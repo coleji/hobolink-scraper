964764a4586b3b9293548c28ea12f73bf9bb0814
Jonathan Cole <jon@jcole.io>
2023-03-25 10:36:25 -0400

    missing files

---
+++ src/db.js
@@ -0,0 +1,47 @@
+var mysql = require("mysql2");
+
+module.exports = function(credentials, readings) {
+	return new Promise(function(resolve, reject) {
+		var connection = mysql.createConnection(credentials);
+		const mappings = {
+			"datetime": "datetime",
+			Temperature: "temp",
+			'Wind Speed': "windSpeed",
+			'Gust Speed': "windGust",
+			'Wind Direction': "windDirDeg",
+			Rain: "rain",
+			'Temperature (Air Temp)': "airTemp",
+			Pressure: "pressure",
+			PAR: "par",
+			RH: "rh",
+			'Dew Point': "dewPoint"
+		}
+		const columns = Object.keys(readings).reduce((agg, label) => {
+			const colName = mappings[label]
+			const {reading, units, extra} = readings[label];
+			if (!colName) {
+				console.log("Unrecognized label " + label)
+				return agg;
+			}
+			agg.statements.push(`${colName} = ?`)
+			agg.values.push(reading)
+			agg.statements.push(`${colName}Unit = ?`)
+			agg.values.push(units)
+			if (colName == "windDirDeg") {
+				agg.statements.push(`windDirSymbol = ?`)
+				agg.values.push(extra)
+			}
+			return agg;
+		}, {statements: [], values: []})
+
+		connection.query(
+			`insert into hobolink_readings set ${columns.statements.join(", ")}`,
+			columns.values,
+			function(err, results) {
+				connection.end();
+				if (err) reject(err);
+				else resolve();
+			}
+		)
+	});
+}
